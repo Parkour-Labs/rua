0deb4de00500588bd1b85cd34f588630bd3a9b2e
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-05-30 11:06:50 +0800

    fix(clean): clean build stuff

--- platforms/rua_dart/target/debug/deps/rua_dart-6d96a72ff6ed00e3.d
+++
@@ -1,5 +0,0 @@
-/Users/xiuxuan/Developer/ParkourLabs/Rust/rua/platforms/rua_dart/target/debug/deps/rua_dart-6d96a72ff6ed00e3.rmeta: src/main.rs
-
-/Users/xiuxuan/Developer/ParkourLabs/Rust/rua/platforms/rua_dart/target/debug/deps/rua_dart-6d96a72ff6ed00e3.d: src/main.rs
-
-src/main.rs:
--- platforms/rua_dart/target/debug/deps/rua_dart-b0c07ef9116e76a0.d
+++
@@ -1,5 +0,0 @@
-/Users/xiuxuan/Developer/ParkourLabs/Rust/rua/platforms/rua_dart/target/debug/deps/rua_dart-b0c07ef9116e76a0.rmeta: src/main.rs
-
-/Users/xiuxuan/Developer/ParkourLabs/Rust/rua/platforms/rua_dart/target/debug/deps/rua_dart-b0c07ef9116e76a0.d: src/main.rs
-
-src/main.rs:
