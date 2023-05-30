826e4715b49865fbaa6a80a6a2362db6a0baea95
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-05-30 11:55:07 +0800

    fix: remove config

--- rua_config/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct RuaConfig {
-    native_entry: Option<String>,
-    platform_entry: Option<String>,
-}
